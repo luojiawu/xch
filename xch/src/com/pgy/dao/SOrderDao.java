090dabe99a688f81824bdf993e7db3751865e918
zhuangjia <1572557889@qq.com>
2020-12-12 18:09:28 +0800

    12.12

--- xch/src/com/pgy/dao/SOrderDao.java
+++ xch/src/com/pgy/dao/SOrderDao.java
@@ -1,51 +1,52 @@
 package com.pgy.dao;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
-import com.pgy.common.Util;
 import com.xch.dto.S_Order;
 
 
 
-public class OrderDao extends BaseDao{
+public class SOrderDao extends BaseDao{
 												  	
 	private static final String NAMESPACE_NAME = "com.xch.mapper.SOrderMapper.";
 	
-	public int count(Map<String,Object> params){
-		//创建数据库用户
-		SqlSession session = getSqlSession();
-		int count = session.selectOne(NAMESPACE_NAME+"count",params);
-		closeSqlSession();
-		return count;
-	}
+	
 	
 	public static void main(String[] args) {
 		
-		OrderDao oDao = new OrderDao();
+		SOrderDao oDao = new SOrderDao();
 		
 		S_Order o = new S_Order();
-		
+		/*o.setMoney(0.04);*/
 		o.setOrderId("11");
-		o.setServerId(1);
-		o.setBooking("11");
-		o.setOrderStatus(1);
-		o.setName("11");
-		o.setPhone("1");
-		oDao.addOrder(o);
+		/*o.setOrderStatus(5);*/
+		
+		
+		
+		o.setStaffId("66");
+		
+		oDao.updateSOrder(o);
 		
 		
 	}
 	
-	public List<S_Order> getPageOrder(){
+	public List<S_Order> getSOrderList(S_Order order){
+		
+		SqlSession session = getSqlSession();
+		List<S_Order> orderList = session.selectList(NAMESPACE_NAME+"getSOrderList",order);
+		closeSqlSession();
+		return orderList;
+		
+	}
+	
+	public S_Order getSOrder(String orderId){
 		//创建数据库用户
 		SqlSession session = getSqlSession();
-		List<S_Order> OrderList = session.selectList(NAMESPACE_NAME+"getMTemplateBookList");
+		S_Order order = session.selectOne(NAMESPACE_NAME+"getSOrder",orderId);
 		closeSqlSession();
-		return OrderList;
+		return order;
 	}
 	
 	public int addOrder(S_Order order){
@@ -61,41 +62,11 @@ public class OrderDao extends BaseDao{
 		return i;
 	}
 	
-	
-	/*public List<Order> getPageOrder(Map<String,Object> params){
-		//创建数据库用户
-		SqlSession session = getSqlSession();
-		List<Order> OrderList = session.selectList(NAMESPACE_NAME+"findByPage",params);
-		closeSqlSession();
-		return OrderList;
-	}
-	
-	public static Order timeContrast(Order order,SqlSession sqlSession) {
-		
-		long l = Long.valueOf(order.getOrderTime()); 
-		
-		long closeTime = l+Util.PAYCLOSETIME*60*60*1000;
-		//如果
-		if(System.currentTimeMillis() > closeTime) {
-			
-			order.setStatus(-1);
-			
-			int i = sqlSession.update(NAMESPACE_NAME+"updateOrderStatus", order);
-			
-			sqlSession.commit();
-			
-		}
-		
-		return order;
-	} 
-	
-	
-	
-	public int addOrder(Order order){
+	public int updateSOrder(S_Order order){
 		
 		SqlSession sqlSession = getSqlSession();
 		
-		int i = sqlSession.insert(NAMESPACE_NAME+"addOrder", order);
+		int i = sqlSession.update(NAMESPACE_NAME+"updateSOrder", order);
 		
 		sqlSession.commit();
 		
@@ -104,115 +75,5 @@ public class OrderDao extends BaseDao{
 		return i;
 	}
 	
-	public Order getOrderByOrderId(String orderId) {
-		
-		SqlSession sqlSession = getSqlSession();
-		
-		Order order = sqlSession.selectOne(NAMESPACE_NAME+"getOrderByOrderId", orderId);
-		
-		if(0 == order.getStatus()) {
-			timeContrast(order, sqlSession);
-		}		
-		
-		closeSqlSession();
-		
-		return order;
-		
-	}
-	
-	
-	public List<Order> getOrderListByOpenId(String openId) {
-		
-		SqlSession sqlSession = getSqlSession();
-		
-		List<Order> orderList = sqlSession.selectList(NAMESPACE_NAME+"getOrderByOpenId", openId);
-		
-		for(int i=0;i<orderList.size();i++) {
-			Order order = orderList.get(i);
-			
-			if(0 == order.getStatus()) {
-				timeContrast(order,sqlSession);
-			} 
-			
-		}
-		
-		closeSqlSession();
-		
-		return orderList;
-		
-	}
-	
-	public int updateOrderStatusByOrderId(String orderId,int status) {
-		
-		SqlSession sqlSession = getSqlSession();
-		
-		HashMap<String, Object> map = new HashMap<>();
-		
-		map.put("orderId", orderId);
-		map.put("status", status);
-		
-		int i = sqlSession.update(NAMESPACE_NAME+"updateOrderStatusByOrderId", map);
-		
-		sqlSession.commit();
-		
-		closeSqlSession();
-		
-		return i;
-		
-	}
-	
-	public int updateOrderStatus(String orderId,String wxOrderId,double payMoney,int goodsNum,String payTime) {
-		
-		Order order = new Order();
-		order.setOrderId(orderId);
-		order.setWxOrderId(wxOrderId);
-		order.setPayMoney(payMoney);
-		order.setGoodsNum(goodsNum);
-		order.setPayTime(payTime);
-		order.setStatus(1);
-		
-		SqlSession sqlSession = getSqlSession();
-		
-		int i = sqlSession.update(NAMESPACE_NAME+"updateOrderStatus", order);
-		
-		sqlSession.commit();
-		
-		closeSqlSession();
-		
-		return i;
-		
-	}
-	
-	public int delOrderByOrderId(String orderId) {
-		
-		SqlSession sqlSession = getSqlSession();
-		
-		int i = sqlSession.delete(NAMESPACE_NAME+"delOrderByOrderId", orderId);
-		
-		sqlSession.commit();
-		
-		closeSqlSession();
-		
-		return i;
-		
-	}
-	
-	public int updateOrderSiteId(String orderId,int siteId) {
-		
-		Order order = new Order();
-		
-		order.setOrderId(orderId);
-		order.setSiteId(siteId);
-		
-		SqlSession sqlSession = getSqlSession();
-		
-		int i = sqlSession.update(NAMESPACE_NAME+"updateOrderSiteId", order);
-		
-		sqlSession.commit();
-		
-		closeSqlSession();
-		
-		return i;
-	}*/
 	
 }
